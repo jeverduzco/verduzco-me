384c7e96f6c4a9dae8d47b4d2220adad3cb2829e
Jesús Verduzco <jesus@verduzco.me>
2020-10-01 19:10:58 -0500

    Next & Prev

---
+++ components/blog/PrevNext.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container
+    grid-list-xl
+    class="content-container"
+  >
+    <v-layout
+      wrap
+      row
+      align-center
+    >
+      <v-flex xs12 md6>
+        <v-card
+          v-if="prev"
+          :to="{ name: 'blog-slug', params: { slug: prev.slug } }"
+          nuxt
+          exact
+          outlined
+          height="125"
+        >
+          <v-card-title primary-title>
+            Más nuevo
+          </v-card-title>
+          <v-card-text>
+            {{ prev.title }}
+          </v-card-text>
+        </v-card>
+        <v-card
+          v-if="!prev"
+          outlined
+          height="125"
+        >
+          <v-card-title primary-title>
+            Más nuevo
+          </v-card-title>
+          <v-card-text>
+            No hay artículos más nuevos.
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 md6>
+        <v-card
+          v-if="next"
+          :to="{ name: 'blog-slug', params: { slug: next.slug } }"
+          nuxt
+          exact
+          outlined
+          height="125"
+        >
+          <v-card-title primary-title>
+            Más antiguo
+          </v-card-title>
+          <v-card-text>
+            {{ next.title }}
+          </v-card-text>
+        </v-card>
+        <v-card
+          v-if="!next"
+          outlined
+          height="125"
+        >
+          <v-card-title primary-title>
+            Más antiguo
+          </v-card-title>
+          <v-card-text>
+            No hay artículos más antiguos.
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+export default {
+  props: {
+    prev: {
+      type: Object,
+      default: () => null
+    },
+    next: {
+      type: Object,
+      default: () => null
+    }
+  }
+}
+</script>
