6cdf0a2d7715329db71d21a8ad3cb030d1a3d371
Jesús Verduzco <jesus@verduzco.me>
2020-10-28 17:10:03 -0600

    Add Microsoft clarity

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,19 @@ export default {
         type: 'image/x-icon',
         href: 'https://storage.verduzco.dev/dotme/website/icon-n-192.png'
       }
-    ]
+    ],
+    script: [
+      {
+        innerHTML: `(function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "3v7yxn0mro");`,
+        type: 'text/javascript',
+        charset: 'utf-8'
+      }
+    ],
+    __dangerouslyDisableSanitizers: ['script', 'innerHTML']
   },
   /*
    ** Global CSS
